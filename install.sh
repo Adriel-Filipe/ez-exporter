05f2628b3cd1119111070a1558fd7d0a93bbafdf
Adriel Filipe <78910163+Adriel-Filipe@users.noreply.github.com>
2021-10-25 14:00:57 -0400

    Decrease in code size
    Still looking for how to solve the CMYK export error with inkporter cli, I believe it works only in version 1.0

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+op1="0"
+op2="0"
+op3="0"
 
 echo "Welcome to installer"
 echo "1) install inkscape"
@@ -9,45 +12,39 @@ echo "4) install all"
 read -p "Option:" opcao
 case "$opcao" in 
 1)
-sudo add-apt-repository ppa:inkscape.dev/stable -y
-sudo apt update -y
-sudo apt install inkscape -y
+op1="1"
 ;;
 2)
-sudo chmod 777 /usr/
-sudo chmod 777 /usr/share/
-sudo chmod 777 /usr/share/inkscape/
-sudo chmod 777 /usr/share/inkscape/extensions/
-sudo cp -r ./inkporter_extension/* /usr/share/inkscape/extensions/
+op2="1"
 ;;
 3)
-curl -s --compressed "https://gimpscape.github.io/gimpscape-ppa/tools/KEY.gpg" | sudo apt-key add -
-sudo curl -s --compressed -o /etc/apt/sources.list.d/gimpscape-ppa.list "https://gimpscape.github.io/gimpscape-ppa/tools/gimpscape-ppa.list"
-sudo apt update -y
-sudo apt install inkporter -y
+op3="1"
 ;;
 4)
-#installing inkscape PPA Version
-sudo add-apt-repository ppa:inkscape.dev/stable -y
-sudo apt update -y
-sudo apt install inkscape -y
-
-#install inkporter extension
-
-sudo chmod 777 /usr/
-sudo chmod 777 /usr/share/
-sudo chmod 777 /usr/share/inkscape/
-sudo chmod 777 /usr/share/inkscape/extensions/
-
-sudo cp -r ./public/inkporter_extension/* /usr/share/inkscape/extensions/
-
-#install inkporter cli 
-curl -s --compressed "https://gimpscape.github.io/gimpscape-ppa/tools/KEY.gpg" | sudo apt-key add -
-sudo curl -s --compressed -o /etc/apt/sources.list.d/gimpscape-ppa.list "https://gimpscape.github.io/gimpscape-ppa/tools/gimpscape-ppa.list"
-sudo apt update -y
-sudo apt install inkporter -y
+op1="1"
+op2="1"
+op3="1"
 ;;
 *)
 echo "Option not find"
 exit 1
 esac 
+
+if [ $op1="1" ]; then
+#installing inkscape PPA Version
+sudo add-apt-repository ppa:inkscape.dev/stable -y
+sudo apt update -y && sudo apt install inkscape -y
+fi
+
+if [ $op2="1" ]; then
+#install inkporter extension
+sudo chmod 777 /usr/ /usr/share/ /usr/share/inkscape/ /usr/share/inkscape/extensions/
+sudo cp -r ./public/inkporter_extension/* /usr/share/inkscape/extensions/
+fi
+
+if [ $op3="3" ]; then
+#install inkporter cli 
+curl -s --compressed "https://gimpscape.github.io/gimpscape-ppa/tools/KEY.gpg" | sudo apt-key add -
+sudo curl -s --compressed -o /etc/apt/sources.list.d/gimpscape-ppa.list "https://gimpscape.github.io/gimpscape-ppa/tools/gimpscape-ppa.list"
+sudo apt update -y && sudo apt install inkporter -y
+fi
