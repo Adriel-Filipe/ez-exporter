768843382ccce50023c6a8258220c86c9aa5a800
Adriel Filipe <78910163+Adriel-Filipe@users.noreply.github.com>
2021-10-25 16:57:23 -0400

    Update ez_exporter.sh

--- ez_exporter.sh
+++ ez_exporter.sh
@@ -12,8 +12,10 @@ echo "eZ-exporter"
 echo "-----------"
 echo ""
 echo "by Adriel Filipe"
-
-read -p "type logo id:" id_logo
+echo "What is name of the .svg file?"
+read -p "file name:" file_svg
+echo "What is your logo ID?"
+read -p "logo ID:" id_logo
 
 #make folders
 if [ ! -d "Logotipo" ]; then
@@ -26,10 +28,11 @@ mv $id_logo ./Logotipo
 mv RGB CMYK ./Logotipo/$id_logo/
 fi
 
-echo "Whats is your logo version?"
-echo "1) Colorfull"
+echo "Whats version of your logo?"
+echo "1) Colorful"
 echo "2) Positive"
 echo "3) Negative"
+echo "for more information about logo version see the README in https://github.com/adriel-filipe/ez-exporter"
 read -p  "logo version:" logo_version
 
 case "$logo_version" in
@@ -49,29 +52,30 @@ esac
 # // RGB EXPORT // #
 
 #PNG
-inkporter logo.svg $id_logo < ./public/export_profiles/$dir_profiles/RGB/png-rgb
+inkporter $file_svg $id_logo < ./public/export_profiles/$dir_profiles/RGB/png-rgb
 mv $id_logo.png "$id_logo-rgb.png" && mv $id_logo-rgb.png ./Logotipo/$id_logo/RGB/
 #JPG 
-inkporter logo.svg $id_logo < ./public/export_profiles/$dir_profiles/RGB/jpg-rgb 
+inkporter $file_svg $id_logo < ./public/export_profiles/$dir_profiles/RGB/jpg-rgb 
 mv $id_logo.jpg "$id_logo-rgb.jpg" && mv $id_logo-rgb.jpg ./Logotipo/$id_logo/RGB
 #SVG
-inkporter logo.svg $id_logo < ./public/export_profiles/$dir_profiles/RGB/svg-rgb
+inkporter $file_svg $id_logo < ./public/export_profiles/$dir_profiles/RGB/svg-rgb
 mv $id_logo.svg "$id_logo-rgb.svg" && mv $id_logo-rgb.svg ./Logotipo/$id_logo/RGB
 #EPS
-inkporter logo.svg $id_logo <./public/export_profiles/$dir_profiles/RGB/eps-rgb
+inkporter $file_svg $id_logo <./public/export_profiles/$dir_profiles/RGB/eps-rgb
 mv $id_logo.eps "$id_logo-rgb.eps" && mv $id_logo-rgb.eps ./Logotipo/$id_logo/RGB
 
 #// CMYK EXPORT // #
 
 #JPG
-inkporter logo.svg $id_logo < ./public/export_profiles/$dir_profiles/CMYK/jpg-cmyk 
+inkporter $file_svg $id_logo < ./public/export_profiles/$dir_profiles/CMYK/jpg-cmyk 
 mv $id_logo.jpg "$id_logo-cmyk.jpg" && mv $id_logo-cmyk.jpg ./Logotipo/$id_logo/CMYK
 #PDF
-inkporter logo.svg $id_logo < ./public/export_profiles/$dir_profiles/CMYK/pdf-cmyk
+inkporter $file_svg $id_logo < ./public/export_profiles/$dir_profiles/CMYK/pdf-cmyk
 mv $id_logo.pdf "$id_logo-cmyk.pdf" && mv $id_logo-cmyk.pdf ./Logotipo/$id_logo/CMYK
 #EPS
-inkporter logo.svg $id_logo < ./public/export_profiles/$dir_profiles/CMYK/eps-cmyk
+inkporter $file_svg $id_logo < ./public/export_profiles/$dir_profiles/CMYK/eps-cmyk
 mv $id_logo.eps "$id_logo-cmyk.eps" && mv $id_logo-cmyk.eps ./Logotipo/$id_logo/CMYK
 #SVG
-inkporter logo.svg $id_logo < ./public/export_profiles/$dir_profiles/CMYK/svg-cmyk
+inkporter $file_svg $id_logo < ./public/export_profiles/$dir_profiles/CMYK/svg-cmyk
 mv $id_logo.svg "$id_logo-cmyk.svg" && mv $id_logo-cmyk.svg ./Logotipo/$id_logo/CMYK
+echo "finished process."
